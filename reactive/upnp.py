75bef719bc05d15bc7a8ffb558bc1d2a33f22f47
Chris Sanders <sanders.chris@gmail.com>
2017-06-24 19:31:59 -0500

    Wrong name on close port function wasn't closing upnp ports

--- reactive/upnp.py
+++ reactive/upnp.py
@@ -11,27 +11,36 @@ config = hookenv.config()
 
 def open_upnp_port(external_port):
     open_port(external_port)
+    external_port = int(external_port)
     if config['enable-upnp']:
         try:
             upnp = miniupnpc.UPnP()
             upnp.discover()
             upnp.selectigd()
-            upnp.addportmapping(external_port,'tcp',upnp.lanaddr,external_port,'juju expose upnp','')
+            if upnp.addportmapping(external_port,'tcp',upnp.lanaddr,external_port,'juju expose upnp',''):
+                hookenv.log('Upnp open successful: {}'.format(external_port),'INFO')
+            else:
+                hookenv.log('Upnp open failed: {}'.format(external_port),'WARNING')
         except Exception as e:
-            hookenv.log('Upnp open failed: {}'.format(e.args[0]),'ERROR')
-                    
+            hookenv.log('Upnp open error: {}'.format(e.args[0]),'ERROR')
+            raise
 
 def close_upnp_port(external_port):
     close_port(external_port)
+    external_port = int(external_port)
     if config['enable-upnp']:
         try:
             upnp = miniupnpc.UPnP()
             upnp.discover()
             upnp.selectigd()
-            upnp.deleteportmapping(external_port,'tcp')
+            if upnp.deleteportmapping(external_port,'tcp'):
+                hookenv.log('Upnp close successful: {}'.format(external_port),'INFO')
+            else:
+                hookenv.log('Upnp close failed: {}'.format(external_port),'WARNING')
         except Exception as e:
-            hookenv.log('Upnp close failed: {}'.format(e.args[0]),'ERROR')
+            hookenv.log('Upnp close error: {}'.format(e.args[0]),'ERROR')
+            raise
 
 hookenv.open_port = open_upnp_port
-hookenv.close_port = close_port
+hookenv.close_port = close_upnp_port
 
