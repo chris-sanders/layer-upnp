2fe2889bd359230a279b82edb21562d8655cfa55
Chris Sanders <sanders.chris@gmail.com>
2017-09-23 15:26:44 -0500

    Fixed pep8

--- reactive/upnp.py
+++ reactive/upnp.py
@@ -1,13 +1,11 @@
-from charms.reactive import when, when_not, set_state
 import miniupnpc
-
 from charmhelpers.core import hookenv
-import miniupnpc
 
 open_port = hookenv.open_port
 close_port = hookenv.close_port
 
 config = hookenv.config()
+
 
 def open_upnp_port(external_port):
     open_port(external_port)
@@ -18,13 +16,14 @@ def open_upnp_port(external_port):
             upnp.discover()
             upnp.selectigd()
             message = "layer-upnp unit {}".format(hookenv.local_unit())
-            if upnp.addportmapping(external_port,'tcp',upnp.lanaddr,external_port,message,''):
-                hookenv.log('Upnp open successful: {}'.format(external_port),'INFO')
+            if upnp.addportmapping(external_port, 'tcp', upnp.lanaddr, external_port, message, ''):
+                hookenv.log('Upnp open successful: {}'.format(external_port), 'INFO')
             else:
-                hookenv.log('Upnp open failed: {}'.format(external_port),'WARNING')
+                hookenv.log('Upnp open failed: {}'.format(external_port), 'WARNING')
         except Exception as e:
-            hookenv.log('Upnp open error: {}'.format(e.args[0]),'ERROR')
+            hookenv.log('Upnp open error: {}'.format(e.args[0]), 'ERROR')
             raise
+
 
 def close_upnp_port(external_port):
     close_port(external_port)
@@ -34,13 +33,14 @@ def close_upnp_port(external_port):
             upnp = miniupnpc.UPnP()
             upnp.discover()
             upnp.selectigd()
-            if upnp.deleteportmapping(external_port,'tcp'):
-                hookenv.log('Upnp close successful: {}'.format(external_port),'INFO')
+            if upnp.deleteportmapping(external_port, 'tcp'):
+                hookenv.log('Upnp close successful: {}'.format(external_port), 'INFO')
             else:
-                hookenv.log('Upnp close failed: {}'.format(external_port),'WARNING')
+                hookenv.log('Upnp close failed: {}'.format(external_port), 'WARNING')
         except Exception as e:
-            hookenv.log('Upnp close error: {}'.format(e.args[0]),'ERROR')
+            hookenv.log('Upnp close error: {}'.format(e.args[0]), 'ERROR')
             raise
+
 
 hookenv.open_port = open_upnp_port
 hookenv.close_port = close_upnp_port
